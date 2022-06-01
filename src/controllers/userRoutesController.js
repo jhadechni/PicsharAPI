4f280061cccf9afe48e0aa329f3fc9612fdc9047
Jaime Sierra <49314853+jhadechni@users.noreply.github.com>
2022-05-31 19:27:50 -0500

    fixing errors

--- src/controllers/userRoutesController.js
+++ src/controllers/userRoutesController.js
@@ -1,6 +1,7 @@
 const controller = {}
-const { verifyTokenBody } = require("../utils/auth")
+const auth = require("../utils/auth")
 const bcrypt = require('bcrypt')
+const userModel = require('../models/userModel')
 
 controller.login = async (req, res) => {
 
@@ -8,8 +9,7 @@ controller.login = async (req, res) => {
 
         if (req.body.username && req.body.password) {
 
-            const user = await userModel.findOne({ username: req.body.username }, '-_v -_id -password')
-
+            const user = await userModel.findOne({ username: req.body.username }, '-_v -_id')
             if (!user) { return res.status(404).json({ message: "Username incorrect", statusCode: 404 }) }
 
             const payload = {
@@ -18,41 +18,35 @@ controller.login = async (req, res) => {
             }
 
             const validPassword = await bcrypt.compare(req.body.password, payload.password);
-
             const token = auth.createToken(payload)
-
             if (!validPassword) { return res.status(404).json({ message: "Password incorrect", statusCode: 404 }) }
-
             return res.status(200).json({ token })
 
         } else {
 
             if (!req.body.token) { return res.sendStatus(400) }
-
-            if (!await verifyTokenBody(req, res)) { return res.status(401).json({ message: 'token no valido', statusCode: 401 }) }
-
+            if (!await auth.verifyTokenBody(req, res)) { return res.status(401).json({ message: 'token no valido', statusCode: 401 }) }
             return res.status(200).json({ token })
         }
     } catch (error) {
+
         console.log(error)
         res.status(500).json({ data: "Server internal error" })
+
     }
 }
 
 controller.register = async (req, res) => {
 
-    if (!req.body.username || !req.body.password || !req.body.email || !req.body.birthdate || !req.body.bio) { return res.sendStatus(400) }
+    if (!req.body.username || !req.body.password || !req.body.email  || !req.body.bio) { return res.sendStatus(400) }
 
     const user = await userModel.findOne({ cedula: req.body.cedula })
 
     try {
 
         if (user) { return res.status(208).json({ message: "User already exist" }) }
-
         const salt = await bcrypt.genSalt(10)
-
         req.body.password = await bcrypt.hash(req.body.password, salt)
-
         await userModel.create(req.body)
 
         const payload = {
@@ -61,13 +55,11 @@ controller.register = async (req, res) => {
         }
 
         const token = auth.createToken(payload)
-
         return res.status(201).json({ token })
 
     } catch (error) {
 
         console.log(error)
-
         return res.status(500).json({ data: "Server internal error", error: error })
     }
 }
@@ -75,6 +67,6 @@ controller.register = async (req, res) => {
 controller.getUser = async (req, res) => {
 
     if (!req.query.user_id) { return res.sendStatus(400) }
-    
+
 }
 module.exports = controller
